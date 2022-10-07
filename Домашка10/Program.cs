d1e390323808781e6bd05a5c83db5fd39d6aa78c
unknown <erbolatsuleev@mail.ru>
2022-10-07 21:49:21 +0600

    domashka10

---
+++ Домашка10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+string a1 = Console.ReadLine();
+int a = int.Parse(a1);
+if(a > 99 && a < 1000) {
+Console.WriteLine("вторая цифра этого числа " + a1[1]);}
+else{
+   Console.WriteLine("введите трехзначное число");
+}
+
