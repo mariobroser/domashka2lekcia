4d710f960cd9df0731ae4b9a7500d66121ab74e9
unknown <erbolatsuleev@mail.ru>
2022-10-07 22:08:34 +0600

    domashka15

---
+++ Домашка15/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Задача 15 ");
+Console.Write("Введи день недели: ");
+string a1 = Console.ReadLine();
+int a = int.Parse(a1);
+if(a > 0 && a < 6) {
+Console.WriteLine("это будни день");}
+
+if(a > 5 && a < 8) {
+   Console.WriteLine("это выходной день");
+}
+if(a > 7 || a < 1)  {
+    Console.WriteLine("такого дня недели нет");
+};
