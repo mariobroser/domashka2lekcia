4dd2a8bdf03a02d223fc8c712966e0ebf4929861
unknown <erbolatsuleev@mail.ru>
2022-10-08 16:58:05 +0600

    domashka23

---
+++ Домашка23/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Задача 23 ");
+int PrintNumber(string message)
+{
+    Console.WriteLine(message);
+    string input = Console.ReadLine();
+    int value = int.Parse(input);
+    return value;
+}
+
+int N = PrintNumber("Введите число");
+
+for(var i=1; i<=N; i+=1) {
+    double a = Math.Pow(i, 3);
+    if (i < N) {
+        Console.Write(a +",");
+    }
+    else 
+    {Console.Write(a+".");}
+}
