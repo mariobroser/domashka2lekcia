d0937774fa1f6c24c269153473820cee5229714d
unknown <erbolatsuleev@mail.ru>
2022-10-14 22:01:29 +0600

    domashka25

---
+++ Домамшка25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите первое число ");
+int number2 = int.Parse(Console.ReadLine());
+int result = 1;
+
+void ShowResult(int number) {
+for(int i=1; i<=number2; i++) {
+    result = result * number1;
+}
+Console.WriteLine(result);
+}
+
+ShowResult(result);
