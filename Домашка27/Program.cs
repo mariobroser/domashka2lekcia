d00e226b57ce9b26277319b31280496096a51747
unknown <erbolatsuleev@mail.ru>
2022-10-17 00:34:22 +0600

    domshka27

---
+++ Домашка27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число ");
+string number1 = Console.ReadLine();
+int n = int.Parse(number1);
+
+int result = 0;
+int f =0;
+
+int NumberLen = number1.Length;
+
+void Summ(int n) {
+    for(int i=0;i<NumberLen;i++) {
+        f=n%10;
+        n=n/10;
+        result=result+f;
+    }
+    Console.WriteLine(result);
+}
+
+Summ(n);
