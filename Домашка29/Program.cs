a3b6e46a9f9ada8a7ca39255f0f63efc062d24f3
unknown <erbolatsuleev@mail.ru>
2022-10-17 00:50:03 +0600

    domashka29

---
+++ Домашка29/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите количество чисел в массиве ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите MAX ");
+int max = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите MIN ");
+int min = int.Parse(Console.ReadLine());
+
+Random mySluchainoeChislo = new Random();
+int[] massiv = new int[n];
+
+void FunctionShow(int n) {
+    for(int i =0; i<massiv.Length;i++){
+        massiv[i] = mySluchainoeChislo.Next(min,max);
+        Console.Write(massiv[i] + " ");
+              
+    }
+}
+
+FunctionShow(n);
