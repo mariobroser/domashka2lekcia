8cb583d70803b47aea4b51c076be3856c31f247f
unknown <erbolatsuleev@mail.ru>
2022-10-19 00:43:14 +0600

    domashka34

---
+++ Домашка34/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите количество чисел в массиве ");
+int n = int.Parse(Console.ReadLine());
+Random mySluchainoeChislo = new Random();
+int[] massiv = new int[n];
+int count = 0;
+
+void FunctionShow(int n) {
+    for(int i =0; i<massiv.Length;i++){
+        massiv[i] = mySluchainoeChislo.Next(100,999);
+        Console.Write(massiv[i] + " ");
+              
+    }
+}
+
+void FunctionCount(int count) {
+    for(int i =0; i<massiv.Length;i++) {
+        if(massiv[i]%2 == 0) {
+            count = count + 1;
+            
+        }
+        else{count = count;}
+    }
+    Console.WriteLine("Количество четных чисел в массиве - " + count);
+
+}
+FunctionShow(n);
+Console.WriteLine();
+FunctionCount(count);
