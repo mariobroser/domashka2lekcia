3efa784b2f8ea00f662b66cdb28aecaac5f3fe0c
unknown <erbolatsuleev@mail.ru>
2022-10-19 00:57:29 +0600

    domashka36

---
+++ Домашка36/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите количество чисел в массиве ");
+int n = int.Parse(Console.ReadLine());
+Random mySluchainoeChislo = new Random();
+int[] massiv = new int[n];
+int sum = 0;
+
+void FunctionShow(int n) {
+    for(int i =0; i<massiv.Length;i++){
+        massiv[i] = mySluchainoeChislo.Next(1,99);
+        Console.Write(massiv[i] + " ");
+              
+    }
+}
+
+void FunctionSumm(int sum) {
+    for(int i =0; i<massiv.Length; i= i+2) {
+        
+            sum = sum + massiv[i];
+            
+        }
+        
+    Console.WriteLine("Сумма чисел стоящих на нечётных позициях. - " + sum);
+
+}
+FunctionShow(n);
+Console.WriteLine();
+FunctionSumm(sum);
