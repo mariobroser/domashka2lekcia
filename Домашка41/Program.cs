dfd61e7acac1561e5a2d071663798184f0fff263
unknown <erbolatsuleev@mail.ru>
2022-10-25 18:04:49 +0600

    domashka41

---
+++ Домашка41/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введит числа через запятую ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int count = 0;
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+
+
+}
+Console.WriteLine("Числа которые вы ввели = " + String.Join(" ", array));
+Console.WriteLine($"Кол-во элементов > 0: {count}");
