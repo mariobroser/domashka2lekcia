d9ff3e04adc61c4c1e88adfc44a46005ccc54241
unknown <erbolatsuleev@mail.ru>
2022-11-09 22:59:33 +0500

    domashka47

---
+++ Домашка 47/Program.cs
@@ -0,0 +1,29 @@
+﻿ int n = Propmt("Введите количество строк");
+ int m = Propmt("Введите количество столбцов");
+
+int Propmt(string message) {
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+
+}
+
+
+double[,] Array = new double[m, n];
+Random rnd = new Random();
+
+void PrintArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { Console.Write($"{matr[i, j]} ");}
+ Console.WriteLine();}}
+
+void FillArray(double[,] matr)
+{ for (int i = 0; i < m; i++)
+ { for (int j = 0; j < n; j++)
+ { matr[i,j] = Convert.ToDouble(rnd.Next(1, 10000)/100.0);}}}
+
+
+FillArray(Array);
+Console.WriteLine();
+PrintArray(Array);
