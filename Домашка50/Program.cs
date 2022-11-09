c024e0db4f0f1c60e6cc6bdc4bcccf7e2b698568
unknown <erbolatsuleev@mail.ru>
2022-11-09 23:09:09 +0500

    domashka50

---
+++ Домашка50/Program.cs
@@ -0,0 +1,33 @@
+﻿ int m = Propmt("Введите количество строк");
+ int n = Propmt("Введите количество столбцов");
+ int Propmt(string message) {
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+
+}
+
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+ 
+  int a = Propmt("Введите координаты масива Х");
+ int b = Propmt("Введите координаты масива У");
+ if (a>m && b>n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = array.GetValue(a,b);
+ Console.WriteLine(c);
+ }
