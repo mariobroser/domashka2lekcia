e1c30bb56f516d86f9a12667678f0e82c9116568
unknown <erbolatsuleev@mail.ru>
2022-11-15 13:53:36 +0500

    domashka64

---
+++ Домашка64/Program.cs
@@ -0,0 +1,21 @@
+﻿int Prompt(string message) {
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int n = Prompt("Введите число");
+    
+    
+    
+    String recursion(int n) {
+        
+        if (n == 1) {
+            return "1";
+        }
+        
+        return n + " " + recursion(n - 1);
+    }
+    
+
+Console.WriteLine(recursion(n));
