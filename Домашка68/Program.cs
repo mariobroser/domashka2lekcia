aacab50fee165f3ea6e31f87bc39f0d35c21d1d2
unknown <erbolatsuleev@mail.ru>
2022-11-15 14:19:05 +0500

    domashka68

---
+++ Домашка68/Program.cs
@@ -0,0 +1,30 @@
+﻿int Prompt(string message) {
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+
+int Akkerman(int m, int n)
+{
+if (m == 0)
+{
+return n + 1;
+}
+else if (n == 0 && m > 0)
+{
+return Akkerman(m - 1, 1);
+}
+else
+{
+return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+}
+
+int m = Prompt("Ввод m: ");
+int n = Prompt("Ввод n: ");
+int akkermanFunction = Akkerman(m, n);
+
+Console.Write($"m = {m}, n = {n} = A(m,n) = {akkermanFunction} ");
+    
+  
